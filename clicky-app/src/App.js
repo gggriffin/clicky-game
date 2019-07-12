ce5a9011ee7dd0755e8304ac31c8255f51d81d50
Garrett Griffin <griffin.garrett@gmail.com>
2019-07-11 19:06:56 -0500

    moved Increment functions to App.js so they are accesible by all components

--- clicky-app/src/App.js
+++ clicky-app/src/App.js
@@ -1,42 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import ParkCard from './components/ParkCard'
-import parks from "./parks.json"
-import Wrapper from "./components/Wrapper"
+import parks from './parks.json'
+import Wrapper from './components/Wrapper'
+import Score from './components/Score'
 import './App.css';
-
+import shuffle from 'shuffle-array'
 class App extends Component {
+
   state = {
+    currentScore: 0,
+    highScore: 0,
     parks
   };
 
+
+  currentIncrement = () => {
+    this.setState({ currentScore: this.state.currentScore + 1 });
+  };
+
+  highIncrement = () => {
+    this.setState({ highScore: this.state.highScore + 1 });
+  };
+
+
   render() {
-  return (
-    <div className="container background p-0 m-0 col-12">
+    shuffle(parks);
 
-      <div className="row text-center">
-        <h1 className="col-8 offset-2 mt-5">Park Pick</h1>
-        <h4 className="col-10 offset-1 mt-3">Click on a park to earn points, but don't click on it more than once.</h4>
-      </div>
 
-      <div className="row">
-        <div className="content col-8 offset-2 mb-5 py-3">
-          <Wrapper>
-            {parks.map(park => (
-              <ParkCard
-                id={park.id}
-                image={park.image}
-                name={park.name}
-              />
-            ))}
-          </Wrapper>
+
+    return (
+      <div className="container background p-0 m-0 col-12">
+
+        <div className="row text-center">
+          <h1 className="col-8 offset-2 mt-5">Park Pick</h1>
+          <h4 className="col-10 offset-1 mt-3">Click on a park to earn points, but don't click on it more than once.</h4>
         </div>
+        <Score currentScore={this.state.currentScore}
+                highScore={this.state.highScore}/>
+        <div className="row">
+          <div className="content col-8 offset-2 mb-5 py-3">
+            <Wrapper>
+              {parks.map(park => (
+                <ParkCard
+                  key={park.id}
+                  image={park.image}
+                  name={park.name}
+                  currentIncrement={this.currentIncrement}
+                  highIncrement={this.highIncrement}
+                />
+              ))}
+            </Wrapper>
+          </div>
+        </div>
+
       </div>
 
-    </div>
-
-  );
-}
+    );
+  }
 }
 
 export default App;
--- clicky-app/src/components/ParkCard/index.js
+++ clicky-app/src/components/ParkCard/index.js
@@ -4,12 +4,14 @@ import './style.css';
 function ParkCard(props) {
     return (
         <div className="card m-2 ml-3 mr-5 col-12 col-md-3 float-right">
-            <div className ="img-fluid mt-2 text-center">
-                <img className ="img" alt={props.name} src={props.image}/>
-            </div>
-            <div className ="content text-center mt-2 mb-1">
-                <h5>{props.name}</h5>
-            </div>
+            <button className="parkBtn" onClick={props.currentIncrement}>
+                <div className="img-fluid mt-2 text-center">
+                    <img className="img" alt={props.name} src={props.image} key={props.id}/>
+                </div>
+                <div className="content text-center mt-2 mb-1">
+                    <h5 className="align-top">{props.name}</h5>
+                </div>
+            </button>
         </div>
     );
 }
